d4d1dc45cead82a8f3a8368286cb2b32828dbbff
Aarchishya Kapoor <aarchishya@gmail.com>
2024-08-15 03:24:29 +0530

    homepage button linked to login page

--- demand_forecast_dashboard/src/Pages/HomePage.js
+++ demand_forecast_dashboard/src/Pages/HomePage.js
@@ -1,34 +1,64 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import homepage_img from "../Assets/homepage_img.jpg";
-import Navbar from '../Components/Navbar';
-
+import Navbar from "../Components/Navbar";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
-    <Navbar/>
-    <Grid container spacing={15} style={{marginLeft:'80px',alignItems:"center"}}>
-      {/* Left Side */}
-      <Grid item xs={8} md={4} style={{ display: 'flex', flexDirection:'column', justifyContent: 'Left',marginTop:'20px',padding:'10px' }}>
-        <Typography variant="h1" style={{ fontWeight: 'bold' }}>
-          Forecast
-        </Typography>
-        <Typography variant="h2" style={{ color: 'gray' }}>
-          Pro
-        </Typography>
-        <Button variant="contained" style={{ backgroundColor: '#FFC220', color: 'white', marginTop: '20px', width: '150px',alignItems:'flex-start' }}>
-          Let’s go
-        </Button>
-      </Grid>
+      <Navbar />
+      <Grid
+        container
+        spacing={15}
+        style={{ marginLeft: "80px", alignItems: "center" }}
+      >
+        {/* Left Side */}
+        <Grid
+          item
+          xs={8}
+          md={4}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "Left",
+            marginTop: "20px",
+            padding: "10px",
+          }}
+        >
+          <Typography variant="h1" style={{ fontWeight: "bold" }}>
+            Forecast
+          </Typography>
+          <Typography variant="h2" style={{ color: "gray" }}>
+            Pro
+          </Typography>
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#FFC220",
+                color: "white",
+                marginTop: "20px",
+                width: "150px",
+                alignItems: "flex-start",
+              }}
+            >
+              Let’s go
+            </Button>
+          </Link>
+        </Grid>
 
-      {/* Right Side */}
-      <Grid item xs={16} md={8}>
-        <img src={homepage_img} alt="Forecast" style={{ width: '73%', height: 'auto',marginTop:'0px' }} />
+        {/* Right Side */}
+        <Grid item xs={16} md={8}>
+          <img
+            src={homepage_img}
+            alt="Forecast"
+            style={{ width: "73%", height: "auto", marginTop: "0px" }}
+          />
+        </Grid>
       </Grid>
-    </Grid>
     </>
   );
 }
